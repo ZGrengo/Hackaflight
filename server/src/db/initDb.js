1f01fa971a7f98c529b3d5a319430802f1f1e543
JosGalSol <josepetgalaxi@gmail.com>
2025-02-13 17:43:23 +0100

    reremodificacion db

--- server/src/db/initDb.js
+++ server/src/db/initDb.js
@@ -125,7 +125,7 @@ const main = async () => {
         console.log('¡Tablas creadas!');
         const hashedPassAdmin = await bcrypt.hash('admin', 10);
         /* const hashedPassUser = await bcrypt.hash('user', 10); */
-
+        /*
         // Insertamos el usuario administrador.
         await pool.query(
             `
@@ -146,7 +146,7 @@ const main = async () => {
                 new Date(),
             ],
         );
-
+*/
         /*
         // Insertamos un usuario normal.
         await pool.query(
