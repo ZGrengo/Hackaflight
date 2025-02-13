36af1e377f536973675e92e43ac8f207d038287b
Angela del Pan <angeladelpan@gmail.com>
2025-02-13 14:59:37 +0100

    sendEmailUtil.js

---
+++ server/src/utils/sendEmailUtil.js
@@ -0,0 +1,36 @@
+//importamos dependencias para el envio de emails
+import nodemailer from 'nodemailer';
+
+//importamos la función que lanza un error
+import generateErrorUtil from './generateErrorUtil.js';
+
+//obtenemos las variables de entorno
+const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS } = process.env;
+
+//Creamos un transporte (una conexión) para poder enviar un email
+const transport = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    auth: {
+        user: SMTP_USER,
+        pass: SMTP_PASS,
+    },
+});
+//Función que envía un email
+const sendEmailUtil = async (email, subject, body) => {
+    try {
+        //Enviamos el email
+        await transport.sendMail({
+            from: SMTP_USER,
+            to: email,
+            subject,
+            text: body,
+        });
+    } catch (err) {
+        console.error(err);
+        generateErrorUtil('Error al enviar el email', 500);
+    }
+};
+
+//exportamos la función
+export default sendEmailUtil;
