727547c58b3bee35750f826fe8e4fc05903a4a78
JosGalSol <josepetgalaxi@gmail.com>
2025-02-13 21:19:28 +0100

    base de datos sin consultas de ejemplo y actualizada a la version stefano 0.1

--- server/src/db/initDb.js
+++ server/src/db/initDb.js
@@ -16,13 +16,12 @@ const main = async () => {
         console.log('Borrando tablas...');
 
         // Borramos las tablas.
-        await pool.query(
-            'DROP TABLE IF EXISTS valorations, itinerary, booking, flies, airports, users',
-        );
+        await pool.query('DROP TABLE IF EXISTS valorations, favorites , users');
 
         console.log('Creando tablas...');
 
-        // Creamos la tabla de usuarios.
+        // Creamos la tabla de usuarios.  FALTA ESTA WEA ; recPassword recCode ; y poner si esta online o offline creo q es opsionale
+
         await pool.query(`
             CREATE TABLE IF NOT EXISTS users (
                 userId INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -34,9 +33,7 @@ const main = async () => {
                 birthdate DATE,
                 avatar VARCHAR(100),
                 role ENUM('admin', 'normal') DEFAULT 'normal',
-                recPassword
-                recCode 
-                online ENUM('online','offline') DEFAULT 'offline'.
+                
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
                 modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
                 
@@ -45,7 +42,6 @@ const main = async () => {
             )
         `);
 
-             
         // Creamos la tabla de criterios de busqueda favoritos de los Usuarios. (En esta tabla pondremos los criterios de busqueda en columnas)
         await pool.query(`
             CREATE TABLE IF NOT EXISTS favorites (
@@ -75,135 +71,7 @@ const main = async () => {
         `);
 
         console.log('¡Tablas creadas!');
-          const hashedPassAdmin = await bcrypt.hash('admin', 10);
-        /* const hashedPassUser = await bcrypt.hash('user', 10); */
-       
-        // Insertamos el usuario administrador.
-        await pool.query(
-            `
-                INSERT INTO users (username, firstname, lastname, email, password, birthdate, avatar, role, payMethod, createdAt, modifiedAt)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            `,
-            [
-                'admin',
-                'admin',
-                'admin',
-                'admin@example.com',
-                hashedPassAdmin,
-                new Date(),
-                'admin',
-                'admin',
-                'Visa',
-                new Date(),
-                new Date(),
-            ],
-        );
-*/
-        
-        // Insertamos un usuario normal.
-        await pool.query(
-            `
-                INSERT INTO users (username, firstname, lastname, email, password, birthdate, avatar, role, payMethod, createdAt, modifiedAt)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            `,
-            [
-                'user',
-                'John',
-                'Doe',
-                'user@example.com',
-                hashedPassUser,
-                new Date('1990-01-01'),
-                'user_avatar.png',
-                'normal',
-                'Paypal',
-                new Date(),
-                new Date(),
-            ],
-        );
 
-        console.log('¡Usuarios insertados!');
-
-        // Insertamos datos de ejemplo en la tabla de aeropuertos.
-        await pool.query(
-            `
-                INSERT INTO airports (name, city, country, createdAt)
-                VALUES (?, ?, ?, ?), (?, ?, ?, ?)
-            `,
-            [
-                'JFK International Airport',
-                'New York',
-                'USA',
-                new Date(),
-                'Heathrow Airport',
-                'London',
-                'UK',
-                new Date(),
-            ],
-        );
-
-        // Insertamos datos de ejemplo en la tabla de vuelos.
-        await pool.query(
-            `
-                INSERT INTO flies (codeFly, airline, origin, destiny, scales, arrivals, departures, price, createdAt)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-            `,
-            [
-                12345,
-                1,
-                1,
-                2,
-                0,
-                new Date('2023-12-01 10:00:00'),
-                new Date('2023-12-01 14:00:00'),
-                500,
-                new Date(),
-            ],
-        );
-
-        // Insertamos datos de ejemplo en la tabla de reservas.
-        await pool.query(
-            `
-                INSERT INTO booking (userId, flyId, reserveDate, title, origin, destiny, scales, place, description, luggage, class, extras, createdAt, modifiedAt)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            `,
-            [
-                2,
-                1,
-                new Date(),
-                'Vacation',
-                1,
-                2,
-                0,
-                '12A',
-                'Vacation trip',
-                1,
-                'EconomyClass',
-                0,
-                new Date(),
-                new Date(),
-            ],
-        );
-
-        // Insertamos datos de ejemplo en la tabla de itinerario.
-        await pool.query(
-            `
-                INSERT INTO itinerary (bookingId, flyId, origin, destiny, scales, duration, createdAt)
-                VALUES (?, ?, ?, ?, ?, ?, ?)
-            `,
-            [1, 1, 1, 2, null, '4 hours', new Date()],
-        );
-
-        // Insertamos datos de ejemplo en la tabla de valoraciones.
-        await pool.query(
-            `
-                INSERT INTO valorations (userId,rate, comment, createdAt)
-                VALUES (?, ?, ?, ?, ?, ?)
-            `,
-            [  , 5, 'Great experience!', new Date()],
-        );
-
-        console.log('¡Datos de ejemplo insertados!');
-    */
         // Cerramos el proceso con código 0.
         process.exit(0);
     } catch (err) {
