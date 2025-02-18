298df14ee1779dd2f05284cffdca8295b82bf86a
Andrés Sarria <andres.sarria@gmail.com>
2025-02-18 16:31:02 +0100

    busqueda Postman

--- server/src/middlewares/joiValidatorMiddleware.js
+++ server/src/middlewares/joiValidatorMiddleware.js
@@ -1,7 +1,7 @@
 import generateErrorUtil from '../utils/generateErrorUtil.js';
 import validateSearch from '../validators/apiValidation.js';
 
-const joiValidatorError = (req, next) => {
+const joiValidatorError = (req, res, next) => {
     const { origin, destination, departureDate, adults } = req.query;
     const { error } = validateSearch.validate({
         originLocationCode: origin,
@@ -11,8 +11,13 @@ const joiValidatorError = (req, next) => {
     });
 
     if (error) {
+        const errorMessage = error.details
+            .map((detail) => detail.message)
+            .join(', ');
         return next(
-            generateErrorUtil('Imposible acceder al servidor amadeus', 500),
+            generateErrorUtil(`Error de validación: ${errorMessage}`, 400),
+            // return next(
+            //     generateErrorUtil('Imposible acceder al servidor amadeus', 500),
         );
     }
     next();
