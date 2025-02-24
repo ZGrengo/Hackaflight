e906b2c4bb290ffa859ac9c5a536725701a8d69f
Angela del Pan <angeladelpan@gmail.com>
2025-02-24 12:37:22 +0100

    ajustes en filterFligth, listado de valoraciones y generateUtilError

--- server/src/controllers/ratings/listRatingsController.js
+++ server/src/controllers/ratings/listRatingsController.js
@@ -1,5 +1,6 @@
 //importamos modelo
 import selectRatingsModel from '../../models/ratings/selectRatingsModel.js';
+import generateErrorUtil from '../../utils/generateErrorUtil.js';
 
 //Función controladora que obtiene el listado de las valoraciones
 const listRatingsController = async (req, res, next) => {
@@ -10,6 +11,15 @@ const listRatingsController = async (req, res, next) => {
         //obtenemos las valoraciones
         const ratings = await selectRatingsModel(rating);
         //console.log('Ratings from DB:', ratings);
+
+        //comprobamos si hay valoraciones con el mismo valor que rating filtrado
+        if (rating && ratings.length === 0) {
+            //si no hay valoraciones con el mismo valor que el rating, lanzamos un error
+            generateErrorUtil(
+                `No se encontraron valoraciones con puntuación ${rating}`,
+                404,
+            );
+        }
         //enviamos una respuesta
         res.send({
             status: 'ok',
