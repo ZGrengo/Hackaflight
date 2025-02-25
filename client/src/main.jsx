d276d19910b0f0e2575896651534a237bc39d18a
JosGalSol <josepetgalaxi@gmail.com>
2025-02-25 18:56:03 +0100

    el main de angela

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+// Importamos la función que indica cuál es la etiqueta raíz del html.
+import { createRoot } from 'react-dom/client';
+
+// Importamos los componentes.
+import App from './App.jsx';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+// Importamos los estilos.
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+);
