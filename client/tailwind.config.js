fbf55ad0535544544b26151f4df5fd4daa0ef529
JosGalSol <josepetgalaxi@gmail.com>
2025-02-25 17:51:29 +0100

    paleta de colores en el tailwind.config

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [],
+    theme: {
+        extend: {},
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            fondo: '#e5f7ff',
+            texto: '#083059',
+            boton: '#3951aa',
+            botohover: '#179dd9',
+        },
+    },
+    plugins: [],
+};
