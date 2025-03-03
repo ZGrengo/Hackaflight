ddcbf09b57d20edf2d6b6f18e3353903347d6eb7
RicardoEA <ricardoea@hotmail.es>
2025-03-03 20:27:50 +0000

    arreglada la busqueda y los filtros, no se actualizaba correctamente

--- client/src/pages/SearchResultsPage.jsx
+++ client/src/pages/SearchResultsPage.jsx
@@ -4,14 +4,23 @@ import { useState, useEffect } from 'react';
 import FlightCard from '../components/FlightCard';
 import FlightFilters from '../components/FlightFilters';
 
-//  obtiene la variable de entorno
+// obtiene la variable de entorno
 const { VITE_API_URL } = import.meta.env;
 
 // definimos la página de resultados de búsqueda
 const SearchResultsPage = () => {
+
+    // obtiene la ubicación actual
     const location = useLocation();
-    const [ flights ] = useState( () => location.state?.flights || [] );
-    const [ filteredFlights, setFilteredFlights ] = useState( flights );
+    const [ flights, setFlights ] = useState( [] );
+    console.log( "Location state:", location.state );
+
+
+    // actualiza los vuelos cuando cambia la ubicación
+    useEffect( () => {
+        setFlights( location.state?.flights || [] );
+        console.log( "Updated flights from location state:", location.state?.flights || [] );
+    }, [ location.state?.flights ] );
 
     console.log( "Initial flights data:", flights );
 
@@ -29,6 +38,8 @@ const SearchResultsPage = () => {
                     searchParams.append( key, filters[ key ] );
                 }
             } );
+            console.log( "Filtered parameters:", searchParams.toString() );
+
 
             // Realizar la petición a la API para vuelos filtrados
             const res = await fetch(
@@ -39,28 +50,26 @@ const SearchResultsPage = () => {
                 }
             );
 
+            console.log( 'Response:', res );
+
             // Manejar la respuesta de la API
             if ( !res.ok ) throw new Error( 'Network response was not ok' );
             const body = await res.json();
 
-            //
+            console.log( 'Response body:', body );
             if ( body.status === 'error' ) throw new Error( body.message );
 
-            const { flights: filteredFlights } = body.data || {};
-            setFilteredFlights( filteredFlights || [] );
+            // Actualizar los vuelos con los datos filtrados
+            const filteredFlights = body.data || [];
+            setFlights( filteredFlights );
 
-            console.log( 'Filtered flights data:', body.data );
+            console.log( 'Filtered flights data:', filteredFlights );
+            console.log( 'Updated flights state:', filteredFlights );
         } catch ( err )
         {
             console.log( 'Error al filtrar vuelos:', err );
         }
     };
-
-    // Actualizar los vuelos filtrados cuando cambie la lista de vuelos
-    useEffect( () => {
-        setFilteredFlights( flights );
-        console.log( "Updated flights data:", flights );
-    }, [ flights ] );
 
     // Mostrar un mensaje si no hay vuelos
     if ( !flights.length )
@@ -68,16 +77,15 @@ const SearchResultsPage = () => {
         return <p>No se encontraron resultados de búsqueda.</p>;
     }
 
-
     // Renderizar la página de resultados de búsqueda
     return (
         <section>
             <FlightFilters onFilterChange={handleFilterChange} />
             <h2>Resultados de la Búsqueda</h2>
             <section className="flight-cards-container">
-                {filteredFlights.length > 0 ? (
-                    filteredFlights.map( ( flight, index ) => (
-                        <FlightCard key={flight.id || index} flight={flight} />
+                {flights.length > 0 ? (
+                    flights.map( ( flight, index ) => (
+                        <FlightCard key={`${ flight.id }-${ index }`} flight={flight} />
                     ) )
                 ) : (
                     <p>No hay vuelos que coincidan con los filtros.</p>
