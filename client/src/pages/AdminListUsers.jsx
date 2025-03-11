d9f5064f7b4d3c69b611e97d807a40d57533b738
JosGalSol <josepetgalaxi@gmail.com>
2025-03-11 14:19:40 +0100

    Estilos copiados, digooo, optimizados

--- client/src/pages/AdminListUsers.jsx
+++ client/src/pages/AdminListUsers.jsx
@@ -8,7 +8,6 @@ import { AuthContext } from '../contexts/AuthContext';
 import Header from '../components/Header.jsx';
 
 // Obtenemos las variables de entorno
-
 const { VITE_API_URL } = import.meta.env;
 
 // Iniciamos el componente
@@ -21,28 +20,18 @@ const AdminListUsers = () => {
     });
 
     // Obtenemos los elementos necesarios del contexto pertinente.
-
     const { users, loading } = useUsersList(searchValues);
     const { authToken } = useContext(AuthContext);
     const navigate = useNavigate();
     const token = authToken;
 
-    // Manejar cambios en los inputs de búsqueda
-    /*const handleChange = (e) => {
-        setSearchValues({
-            ...searchValues,
-            [e.target.name]: e.target.value,
-        });
-    };*/
+    // Estado para manejar la lista de usuarios en la UI
+    const [usersList, setUsersList] = useState([]);
 
-
-        // Estado para manejar la lista de usuarios en la UI
-        const [usersList, setUsersList] = useState([]);
-
-        // Actualizar la lista local de usuarios cuando `users` cambie
-        useEffect(() => {
-            setUsersList(users);
-        }, [users]);
+    // Actualizar la lista local de usuarios cuando `users` cambie
+    useEffect(() => {
+        setUsersList(users);
+    }, [users]);
 
     // Habilitar/Deshabilitar usuario
     const handleToggleUserStatus = async (userId, isActive) => {
@@ -55,17 +44,17 @@ const AdminListUsers = () => {
                 },
                 body: JSON.stringify({ isActive: !isActive })
             });
-    
+
             const data = await res.json();
             if (!res.ok) throw new Error(data.message);
-    
+
             // Actualizar el estado del local users
             setUsersList((prevUsers) =>
                 prevUsers.map((user) =>
                     user.userId === userId ? { ...user, isActive: !isActive } : user
                 )
             );
-    
+
             toast.success(`Usuario ${!isActive ? 'habilitado' : 'deshabilitado'} correctamente.`);
         } catch (error) {
             toast.error(`Error: ${error.message || 'No se pudo actualizar el usuario.'}`);
@@ -77,16 +66,16 @@ const AdminListUsers = () => {
         if (!window.confirm('¿Estás seguro de eliminar este usuario?')) return;
 
         try {
-            const res = await fetch(`${VITE_API_URL}/api/admin/users/${userId}`,
-                 {  method: 'DELETE',
-                     headers: { Authorization: `${authToken}` } },);
+            const res = await fetch(`${VITE_API_URL}/api/admin/users/${userId}`, {
+                method: 'DELETE',
+                headers: { Authorization: `${authToken}` },
+            });
 
             const data = await res.json();
             if (!res.ok) throw new Error(data.message);
 
-
-              // Actualizar la lista de usuarios en el estado local
-              setUsersList((prevUsers) =>
+            // Actualizar la lista de usuarios en el estado local
+            setUsersList((prevUsers) =>
                 prevUsers.map((user) =>
                     user.userId === userId ? { ...user, isActive: !isActive } : user
                 )
@@ -111,87 +100,62 @@ const AdminListUsers = () => {
     return (
         <>
             <Header />
-            <main>
-             
-{/*
-                <div>
-                    <input
-                        type='text'
-                        name='username'
-                        placeholder='Buscar por usuario'
-                        value={searchValues.username}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type='text'
-                        name='email'
-                        placeholder='Buscar por email'
-                        value={searchValues.email}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type='text'
-                        name='firstName'
-                        placeholder='Buscar por nombre'
-                        value={searchValues.firstName}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type='text'
-                        name='lastName'
-                        placeholder='Buscar por apellido'
-                        value={searchValues.lastName}
-                        onChange={handleChange}
-                    />
+            <main className="bg-gradient-to-b from-dark-blue to-white min-h-screen flex flex-col justify-between">
+                <div className="flex flex-col items-center justify-center flex-1 p-4">
+                    <div className="bg-white p-6 sm:p-8 rounded-lg shadow-md w-full max-w-lg lg:max-w-4xl transition transform hover:scale-[1.008]">
+                        <h2 className="text-3xl sm:text-4xl font-heading font-light text-dark-blue text-center mb-6">
+                            Lista de Usuarios
+                        </h2>
+
+                        {loading ? (
+                            <p className="text-center text-dark-blue">Cargando usuarios...</p>
+                        ) : (
+                            <div className="overflow-x-auto">
+                                <table className="min-w-full border border-blue-300 table-fixed">
+                                    <thead>
+                                        <tr className="bg-blue-100">
+                                            <th className="px-4 py-2 border-b text-center">Usuario</th>
+                                            <th className="px-4 py-2 border-b text-center">Email</th>
+                                            <th className="px-4 py-2 border-b text-center">Nombre</th>
+                                            <th className="px-4 py-2 border-b text-center">Apellido</th>
+                                            <th className="px-4 py-2 border-b text-center">Estado</th>
+                                            <th className="px-4 py-2 border-b text-center">Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {usersList.map((user) => (
+                                            <tr key={user.userId} className="odd:bg-white even:bg-blue-50">
+                                                <td className="px-4 py-2 border-b text-center">{user.username}</td>
+                                                <td className="px-4 py-2 border-b text-center">{user.email}</td>
+                                                <td className="px-4 py-2 border-b text-center">{user.firstName}</td>
+                                                <td className="px-4 py-2 border-b text-center">{user.lastName}</td>
+                                                <td className="px-4 py-2 border-b text-center">
+                                                    {user.isActive ? 'Activo' : 'Inactivo'}
+                                                </td>
+                                                <td className="px-4 py-2 border-b text-center">
+                                                    <button
+                                                        onClick={() =>
+                                                            handleToggleUserStatus(user.userId, user.isActive)
+                                                        }
+                                                        className="bg-medium-blue text-white px-4 py-2 rounded hover:bg-medium-blue w-full mb-2"
+                                                    >
+                                                        {user.isActive ? 'Deshabilitar' : 'Habilitar'}
+                                                    </button>
+                                                    <button
+                                                        onClick={() => handleDeleteUser(user.userId)}
+                                                        className="bg-dark-blue text-white px-4 py-2 rounded hover:bg-dark-blue w-full"
+                                                    >
+                                                        Eliminar
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        )}
+                    </div>
                 </div>
-
-                */}
-
-{loading ? (
-    <p className='text-center'>Cargando usuarios...</p>
-) : (
-    <table className="min-w-full border border-blue-300 table-fixed">
-        <thead>
-            <tr className="bg-blue-100 flex">
-                <th className="px-4 py-2 border-b flex-1 text-center">Usuario</th>
-                <th className="px-4 py-2 border-b flex-1 text-center">Email</th>
-                <th className="px-4 py-2 border-b flex-1 text-center">Nombre</th>
-                <th className="px-4 py-2 border-b flex-1 text-center">Apellido</th>
-                <th className="px-4 py-2 border-b flex-1 text-center">Estado</th>
-                <th className="px-4 py-2 border-b flex-1 text-center">Acciones</th>
-            </tr>
-        </thead>
-        <tbody className="flex flex-col">
-            {usersList.map((user) => (
-                <tr key={user.userId} className="odd:bg-white even:bg-blue-50 flex">
-                    <td className="px-4 py-2 border-b flex-1 text-center">{user.username}</td>
-                    <td className="px-4 py-2 border-b flex-1 text-center">{user.email}</td>
-                    <td className="px-4 py-2 border-b flex-1 text-center">{user.firstName}</td>
-                    <td className="px-4 py-2 border-b flex-1 text-center">{user.lastName}</td>
-                    <td className="px-4 py-2 border-b flex-1 text-center">
-                        {user.isActive ? 'Activo' : 'Inactivo'}
-                    </td>
-                    <td className="px-4 py-2 border-b flex-1 flex justify-center">
-                        <button
-                            onClick={() =>
-                                handleToggleUserStatus(user.userId, user.isActive)
-                            }
-                            className="bg-medium-blue text-white px-4 py-2 rounded hover:bg-medium-blue flex-1"
-                        >
-                            {user.isActive ? 'Deshabilitar' : 'Habilitar'}
-                        </button>
-                        <button
-                            onClick={() => handleDeleteUser(user.userId)}
-                            className="bg-dark-blue text-white px-4 py-2 rounded hover:bg-dark-blue ml-2 flex-1"
-                        >
-                            Eliminar
-                        </button>
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-    </table>
-                )}
             </main>
         </>
     );
